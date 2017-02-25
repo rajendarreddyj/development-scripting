a8619e9a24ddc9a431a770fe780ae431a4612b66
rajendarreddy jagapathi <rajendarreddyj@gmail.com>
2017-02-25 15:34:47 -0600

    adding encodefile script

---
+++ shell/basics/shell-projects/encodefile.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+#
+# Script to print user information who currently login , current date & time
+# Rajendarreddy Jagapathi - 23/Feb/2017
+# Usage
+# chmod +x encodefile.sh
+# bash encodefile.sh
+#enter input encoding here
+FROM_ENCODING="value_here"
+#output encoding(UTF-8)
+TO_ENCODING="UTF-8"
+#convert
+CONVERT=" iconv  -f   $FROM_ENCODING  -t   $TO_ENCODING"
+#loop to convert multiple files 
+for  file  in  *.txt; do
+$CONVERT   "$file"   -o  "${file%.txt}.utf8.converted"
+done
+exit 0
